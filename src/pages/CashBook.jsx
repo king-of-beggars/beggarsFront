0e01b22a53dae6b8fa911d6fd9a7ce41f18b9bdf
Danee12 <9356471@gmail.com>
2023-06-21 21:15:13 +0900

    cashBook.jsx png 적용

--- src/pages/CashBook.jsx
+++ src/pages/CashBook.jsx
@@ -9,6 +9,7 @@ import { Box, boxSample } from "assets";
 import { BackgroundDiv } from "styles/styled-components/styles";
 import TestBox from "components/ui/box/TestBox";
 import { DateBox } from "assets";
+import backgroundCash from "assets/pixels/cashbook/cashbook_date_box.png";
 
 function CashBook() {
   // 가계부 date
@@ -18,54 +19,75 @@ function CashBook() {
   const getScreenSize = () => {
     const { innerWidth: width, innerHeight: height } = window;
     return { width, height };
-  }
+  };
 
   const { width, height } = getScreenSize();
-  console.log(height)
+  console.log(height);
 
   const cashbookApiRes = {
-    data:[{
-      id: 0,
-      cashbookCategory: "식비",
-      cashbookName: "아침",
-      cashbookNowValue: 4000,
-      cashbookGoalValue: 8000,
-      cashbookOrder: 0
-    },{
-      id: 1,
-      cashbookCategory: "식비",
-      cashbookName: "점심",
-      cashbookNowValue: 12000,
-      cashbookGoalValue: 15000,
-      cashbookOrder: 1
-    }, {
-      id: 2,
-      cashbookCategory: "식비",
-      cashbookName: "저녁",
-      cashbookNowValue: 0,
-      cashbookGoalValue: 12000,
-      cashbookOrder: 2
-    }, {
-      id: 3,
-      cashbookCategory: "간식비 / 카페",
-      cashbookName: "",
-      cashbookNowValue: 3000,
-      cashbookGoalValue: 5000,
-      cashbookOrder: 3
-    }]
-  }
+    data: [
+      {
+        id: 0,
+        cashbookCategory: "식비",
+        cashbookName: "아침",
+        cashbookNowValue: 4000,
+        cashbookGoalValue: 8000,
+        cashbookOrder: 0,
+      },
+      {
+        id: 1,
+        cashbookCategory: "식비",
+        cashbookName: "점심",
+        cashbookNowValue: 12000,
+        cashbookGoalValue: 15000,
+        cashbookOrder: 1,
+      },
+      {
+        id: 2,
+        cashbookCategory: "식비",
+        cashbookName: "저녁",
+        cashbookNowValue: 0,
+        cashbookGoalValue: 12000,
+        cashbookOrder: 2,
+      },
+      {
+        id: 3,
+        cashbookCategory: "간식비 / 카페",
+        cashbookName: "",
+        cashbookNowValue: 3000,
+        cashbookGoalValue: 5000,
+        cashbookOrder: 3,
+      },
+    ],
+  };
 
   return (
     <>
-      <header style={{ position: "fixed", display: "flex", alignItems: "center", }}>
-        <div className="heading" style={{ fontSize: "1.56em" }}>가계부</div>
+      <header
+        style={{ position: "fixed", display: "flex", alignItems: "center" }}
+      >
+        <div className="heading" style={{ fontSize: "1.56em" }}>
+          가계부
+        </div>
       </header>
-      <div className="cashBookWrapper" style={{
-        display: "flex",
-        flexDirection: "column",
-        border: "2px solid black"
-      }}>
-        <div className="dayWrapper">
+      <div
+        className="cashBookWrapper"
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          border: "2px solid black",
+        }}
+      >
+        <div
+          className="dayWrapper"
+          style={{
+            backgroundSize: "100% 100%",
+            backgroundRepeat: "no-repeat",
+            width: "155px",
+            height: "36px",
+            backgroundImage: `url(${backgroundCash})`,
+          }}
+        >
           <DayPicker
             selectDate={selectDate}
             setSelectDate={setSelectDate}
@@ -75,7 +97,6 @@ function CashBook() {
         </div>
         {cashbookApiRes.data.map((v, i) => {
           if (i === 0) {
-
           }
         })}
       </div>
