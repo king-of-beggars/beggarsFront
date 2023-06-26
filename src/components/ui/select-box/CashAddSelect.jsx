d0ce027557ce3df33f8e10fdf8850bc281fe7756
Danee12 <9356471@gmail.com>
2023-06-26 22:45:03 +0900

    cash book state 추가

--- src/components/ui/select-box/CashAddSelect.jsx
+++ src/components/ui/select-box/CashAddSelect.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import { style } from "styles";
 
-function CashAddSelect({ title, options, placeholder }) {
+function CashAddSelect({ title, options, placeholder, onChange, name, value }) {
   return (
     <style.CashBookCardWrap>
       <style.CashBookHead>{title}</style.CashBookHead>
-      <style.CashBookSelect>
+      <style.CashBookSelect name={name} onChange={onChange} value={value}>
         <option disabled hidden selected>
           {placeholder}
         </option>
-        {options.map((option)=> {
-            return (
-                <option value={option.value} key={option.value}>{option.name}</option>
-            )
+        {options.map((option) => {
+          return (
+            <option value={option.value} key={option.value}>
+              {option.name}
+            </option>
+          );
         })}
       </style.CashBookSelect>
     </style.CashBookCardWrap>
