14fff93abf0edb949766e3d5ca23075e055c72d2
dev-deeplake <127172647+dev-deeplake@users.noreply.github.com>
2023-06-30 19:45:46 +0900

    카드 컴포넌트 분리 및 반응형 조절 완료

--- src/components/ui/box/CardBox.jsx
+++ src/components/ui/box/CardBox.jsx
@@ -1,25 +1,40 @@
 import React from 'react'
 import { style, layout } from "styles"
+import { ProgressBarSemiCircle } from "components"
 
 function CardBox({id, budget, spend, category, title, screenWidth, ratio, isDefault=true}) {
     const [cardWidthOrigin, cardHeightOrigin] = [301, 356]
     const [screenWidthOrigin, screenHeightOrigin] = [393, 852]
-    const cardWidth = ((screenWidth / screenWidthOrigin) * cardWidthOrigin) * ratio
+    const cardWidth = screenWidth > 393 ? cardWidthOrigin * ratio : ((screenWidth / screenWidthOrigin) * cardWidthOrigin) * ratio
     const cardHeight = cardWidth * (cardHeightOrigin / cardWidthOrigin)
+
+    const [bigCardBtnWidth, bigCardBtnHeight] = [274, 46]
+    const [smallCardBtnWidth, smallCardBtnHeight] = [151, 26]
   return (
-    <style.CardBoxContainer cardWidth={cardWidth} cardHeight={cardHeight}>
+    <style.CardBoxContainer ratio={ratio} isDefault={isDefault} cardWidth={`${cardWidth}px`} cardHeight={`${cardHeight}px`}>
         {
             !!title ? (
-                    <style.CardCategoryContainer lineColor={lineColor}>
-                        <style.FirstCategoryText>{category}</style.FirstCategoryText>
-                        <style.SecondCategoryText>{title}</style.SecondCategoryText>
+                    <style.CardCategoryContainer ratio={ratio} isDefault={isDefault}>
+                        <style.Card1stCategoryText ratio={ratio} isDefault={isDefault}>{category}</style.Card1stCategoryText>
+                        <style.Card2ndCategoryText ratio={ratio} isDefault={isDefault}>{title}</style.Card2ndCategoryText>
+                        <style.CardDivision ratio={ratio} isDefault={isDefault} />
+                        <style.CardBudgetText isDefault={isDefault}>매일 {budget}원</style.CardBudgetText>
                     </style.CardCategoryContainer>
                 ) : (
-                    <style.CardCategoryContainer lineColor={lineColor}>
-                    <style.SecondCategoryText style={{margin: "1.5em 0 0.5em 0"}}>{category}</style.SecondCategoryText>
+                    <style.CardCategoryContainer ratio={ratio} isDefault={isDefault}>
+                        <style.Card2ndCategoryText ratio={ratio} isDefault={isDefault} style={{margin: `${1.5 * ratio}em 0 ${0.5 * ratio}em 0`}}>{category}</style.Card2ndCategoryText>
+                        <style.CardDivision ratio={ratio} isDefault={isDefault} />
+                        <style.CardBudgetText isDefault={isDefault}>매일 {budget}원</style.CardBudgetText>                    
                     </style.CardCategoryContainer>
-                ) 
+                )
         }
+      <style.CardProgressBarContainer id="container" ratio={ratio} isDefault={isDefault}>
+        <ProgressBarSemiCircle ratio={ratio} isDefault={isDefault} spend={spend} budget={budget}/>
+      </style.CardProgressBarContainer>
+      <style.CardSpendText ratio={ratio} isDefault={isDefault}>{spend}원 사용</style.CardSpendText>
+      { (!!isDefault && ratio == 0.9)
+        ? <style.CardBtn id={id} ratio={ratio} isDefault={isDefault} btnWidth={`${bigCardBtnWidth * ratio}px`} btnHeight={`${bigCardBtnHeight * ratio}px`}>{spend > budget ? "혼쭐나러 가기" : "자랑하러 가기"}</style.CardBtn>
+        : <style.CardBtn id={id} ratio={ratio} isDefault={isDefault} btnWidth={`${smallCardBtnWidth * ratio}px`} btnHeight={`${smallCardBtnHeight * ratio}px`}>자세히 보기</style.CardBtn>}
     </style.CardBoxContainer>
   )
 }
---
+++ src/components/ui/progress/ProgressBarTmp.jsx
@@ -0,0 +1,132 @@
+import React, { useRef, useEffect } from 'react'
+import ProgressBar from "progressbar.js"
+import * as sVar from "constants/styleVariables"
+
+// 에러 테스트를 위해 생성해놓은 과거 프로그레스 바 코드입니다.
+
+function ProgressBarTmp( { type, budget, spend, ratio }) {
+    const progressBarRef = useRef(null)
+
+    useEffect(() => {
+        const graphValue = (Math.round((spend / budget) * 10)) / 10 * 100
+        console.log("origin:::", Math.round((spend / budget) * 10) / 10)
+        if (type === "semiCircle") {
+          const semiCircle = new ProgressBar.SemiCircle(progressBarRef.current, {
+            strokeWidth: 6,
+            color: `${sVar.textGray}`,
+            trailColor: `${sVar.lightGray}`,
+            trailWidth: 8,
+            easing: 'easeInOut',
+            duration: 1400,
+            svgStyle: null,
+            text: {
+              value: "0%",
+              alignToBottom: false,
+            },
+            from: { color: `${sVar.lightYellow}` },
+            to: { color: `${sVar.strongYellow}` },
+            step: (state, bar) => {
+              bar.path.setAttribute('stroke', state.color);
+              const value = graphValue;
+              // console.log("value::", graphValue)
+              if (value === 0) {
+                bar.setText("0%");
+                // bar.animate(0)
+                bar.text.style.color = `${sVar.lightGray}`;
+              } else if (value > 100 ){
+                bar.setText(`${value}%`);
+                // bar.animate(graphValue / 100)
+                bar.text.style.color = `tomato`;
+                bar.path.setAttribute('stroke', "tomato");
+                bar.trail.setAttribute("stroke", `${sVar.middleYellow}`)
+              } else {
+                bar.setText(`${value}%`);
+                bar.text.style.color = `${sVar.middleYellow}`;
+              }
+            //   bar.text.style.color = `${sVar.middleYellow}`;
+            },
+          });
+          semiCircle.text.style.fontFamily = '"Raleway", Helvetica, sans-serif';
+          semiCircle.text.style.fontSize = `${1 * ratio}em`;
+          
+          if (graphValue > 100) {
+            console.log(graphValue / 100)
+            semiCircle.animate((graphValue / 100) - 1);
+          } else {
+            semiCircle.animate(graphValue / 100);
+          }
+
+
+            return () => {
+                semiCircle.destroy();
+              };
+        } else if (type === "bar") {
+            const bar = new ProgressBar.Line(progressBarRef.current, {
+                strokeWidth: 4,
+                easing: 'easeInOut',
+                duration: 1400,
+                color: '#FFEA82',
+                trailColor: '#eee',
+                trailWidth: 1,
+                svgStyle: {width: '100%', height: '100%'},
+                text: {
+                  style: {
+                    // Text color.
+                    // Default: same as stroke color (options.color)
+                    color: '#999',
+                    position: 'absolute',
+                    right: '0',
+                    top: '30px',
+                    padding: 0,
+                    margin: 0,
+                    transform: null
+                  },
+                  autoStyleContainer: false
+                },
+                from: {color: '#FFEA82'},
+                to: {color: '#ED6A5A'},
+                step: (state, bar) => {
+                  bar.setText(Math.round(bar.value() * 100) + ' %');
+                }
+            });
+            
+            bar.animate(1.0)
+
+            return () => {
+                bar.destroy();
+              };
+        } else if (type === "circle") {
+            const circle = new ProgressBar.Circle(progressBarRef.current, {
+                color: '#FFEA82',
+                trailColor: '#eee',
+                trailWidth: 1,
+                duration: 1400,
+                easing: 'bounce',
+                strokeWidth: 6,
+                from: {color: '#FFEA82', a:0},
+                to: {color: '#ED6A5A', a:1},
+                // Set default step function for all animate calls
+                step: function(state, circle) {
+                  circle.path.setAttribute('stroke', state.color);
+                  var value = Math.round(circle.value() * 100);
+                  if (value === 0) {
+                    circle.setText('');
+                  } else {
+                    circle.setText(value);
+                  }
+                }
+            });
+            circle.text.style.fontFamily = '"Raleway", Helvetica, sans-serif';
+            circle.text.style.fontSize = '2rem';
+
+            circle.animate(1.0);
+            return () => {
+                circle.destroy();
+              };
+            }
+        }, []);
+
+        return <div ref={progressBarRef} />;
+      };
+
+export default ProgressBarTmp
