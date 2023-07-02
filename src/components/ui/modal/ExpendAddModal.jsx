cb0e233c4ffbae3e3ed53bd4bbbe2420ade230da
Danee12 <9356471@gmail.com>
2023-07-02 16:59:09 +0900

    상세 지출 api 적용

--- src/components/ui/modal/ExpendAddModal.jsx
+++ src/components/ui/modal/ExpendAddModal.jsx
@@ -1,13 +1,16 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
 import { useState } from "react";
 
 import { style, layout } from "styles";
 import { BackCramps } from "assets";
 import CashBookInput from "components/ui/input/CashBookInput";
 import { CashBookBtn } from "styles/styled-components/styles";
+import { CashBookAPI } from "api/api";
+import { useMutation } from "react-query";
 
-function ExpendAddModal({ setClose }) {
+function ExpendAddModal({ setClose, cardId }) {
+  const navigate = useNavigate();
   const INIT_INPUT_VALUE = "";
   // 상세 지출 내역 state
   const [expendInfo, setExpendInfo] = useState({
@@ -29,11 +32,33 @@ function ExpendAddModal({ setClose }) {
     }
 
     const newExpend = {
-      ...expendName,
+      ...expendInfo,
       [name]: value,
     };
 
     setExpendInfo(newExpend);
+    console.log(newExpend)
+  };
+
+  // 지출 상세 내역 저장
+  const mutation = useMutation(CashBookAPI.postCashDetailAdd, {
+    onSuccess: (response) => {
+      navigate(`/cash-book/${cardId}`);
+    },
+    onError: () => alert("지출 내역 저장에 실패했습니다."),
+  });
+
+  const onClickSave = () => {
+    if (!expendName | !expendPrice) {
+      alert("지출 상세 내역을 모두 입력해주세요.");
+    } else {
+      const newExpend = {
+        cashDetailText: expendName,
+        cashDetailValue: Number(expendPrice.replace(",", "")),
+      };
+      // alert(newExpend.cashDetailText);
+      mutation.mutate(cardId, newExpend)
+    }
   };
 
   return (
@@ -64,7 +89,9 @@ function ExpendAddModal({ setClose }) {
             value={expendPrice}
             onChange={onChangeInput}
           />
-          <CashBookBtn marginTop="10px">저장</CashBookBtn>
+          <CashBookBtn marginTop="10px" onClick={onClickSave}>
+            저장
+          </CashBookBtn>
         </layout.FlexCenterColumn>
       </style.Modal>
     </style.ModalBackground>
