b043009235b29cd7f60c760ade353f3d503bba68
dev-deeplake <127172647+dev-deeplake@users.noreply.github.com>
2023-07-02 14:11:20 +0900

    BackRenderer 함수 추가 (isLoading, isError 핸들러)

--- src/pages/Board.jsx
+++ src/pages/Board.jsx
@@ -6,48 +6,6 @@ import { useQuery, useQueryClient } from "react-query";
 import { mainBackgroundTop, mainBackgroundMiddle, mainBackgroundTail, backgroundBrightTop, backgroundDarkTop, backgroundBrightMiddle, backgroundDarkMiddle, backgroundBrightTail, backgroundDarkTail } from 'assets';
 import instance, { boardAPI } from 'api/api';
 
-const dummies = {
-  data: [
-    {
-      id: 0,
-      cashbookCategory: "식비",
-      cashbookName: "아침",
-      cashbookNowValue: 4000,
-      cashbookGoalValue: 8000,
-      cashbookOrder: 0,
-      date: "2023-06-28"
-    },
-    {
-      id: 1,
-      cashbookCategory: "식비",
-      cashbookName: "점심",
-      cashbookNowValue: 12000,
-      cashbookGoalValue: 15000,
-      cashbookOrder: 1,
-      date: "2023-06-28"
-    },
-    {
-      id: 2,
-      cashbookCategory: "식비",
-      cashbookName: "저녁",
-      cashbookNowValue: 0,
-      cashbookGoalValue: 12000,
-      cashbookOrder: 2,
-      date: "2023-06-29"
-      
-    },
-    {
-      id: 3,
-      cashbookCategory: "간식비 / 카페",
-      cashbookName: "",
-      cashbookNowValue: 6000,
-      cashbookGoalValue: 5000,
-      cashbookOrder: 3,
-      date: "2023-06-30"
-    },
-  ],
-};
-
 function Board({ isMobile, headerHeight, navHeight, mainHeight }) {
   // card 크기 결정
   // const cardWidth = 301 * 0.5
@@ -67,6 +25,48 @@ function Board({ isMobile, headerHeight, navHeight, mainHeight }) {
     setIsBoasting(!isBoasting)
   }
   
+  const backRenderer = (responseType) => { // 내용 로딩이 덜 되었을 때 보여줄 렌더러, 응답에 따라 내용을 다르게 설정하기
+    return (
+        <style.BackgroundPageLayout
+          screenWidth={`${screenWidth}px`}
+          isMobile={isMobile}
+          backPngTop={isBoasting ? `url(${backgroundBrightTop})` : `url(${backgroundDarkTop})`}
+          backPngTail={isBoasting ? `url(${backgroundBrightTail})` : `url(${backgroundDarkTail})`}
+          backPngMiddle={isBoasting ? `url(${backgroundBrightMiddle})` : `url(${backgroundDarkMiddle})`}>
+        <layout.Header headerHeight={`${headerHeight}px`}>
+          <layout.HeaderContent>
+              { isBoasting
+                ? (
+                  <style.BoardBtnBar>
+                    <style.BoardBtnActivate isBoasting={isBoasting}>자랑하기</style.BoardBtnActivate>
+                    <style.BoardBtnSleep onClick={toggleBtnHandler} isBoasting={isBoasting}>혼쭐나기</style.BoardBtnSleep>
+                  </style.BoardBtnBar>
+                ) : (
+                  <style.BoardBtnBar>
+                    <style.BoardBtnSleep onClick={toggleBtnHandler} isBoasting={isBoasting}>자랑하기</style.BoardBtnSleep>
+                    <style.BoardBtnActivate isBoasting={isBoasting}>혼쭐나기</style.BoardBtnActivate>
+                  </style.BoardBtnBar>
+                )
+              }
+          </layout.HeaderContent>
+        </layout.Header>
+        <layout.Main headerHeight={`${headerHeight}px`} mainHeight={`${mainHeight}px`}>
+          <layout.MainContent>
+            { /* 아래로 response type에 따른 backRenderer 호출 조건 변경 추가 예정 */ }
+            { responseType === "" 
+              ? null
+              : null
+            }
+          </layout.MainContent>
+        </layout.Main>
+        <layout.Nav navHeight={`${navHeight}px`}>
+          <Nav selected="board" />
+        </layout.Nav>
+      </style.BackgroundPageLayout>
+    )
+  }
+
+
   const { 
     data: cardList,
     isLoading,
@@ -75,6 +75,12 @@ function Board({ isMobile, headerHeight, navHeight, mainHeight }) {
     onSuccess: (res) => {
       console.log("response:::", res)
     },
+    isLoading: (res) => {
+      console.log("isLoading:::", res)
+    },
+    isError: (res) => {
+      console.log("isError:::", res)
+    }
   })
 
   if (isLoading) {
