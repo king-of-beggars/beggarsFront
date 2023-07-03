08582315efabcac80dee7633a4181e94b9cf75b9
Danee12 <9356471@gmail.com>
2023-07-03 23:54:37 +0900

    가계부 상세목록 모달 수정 2

--- src/components/ui/modal/CashDetailModal.jsx
+++ src/components/ui/modal/CashDetailModal.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { style, layout } from "styles"
 import * as sVar from "constants/styleVariables.js";
+// import { CashBookNoneBtn } from "styles/styled-components/styles";
 
 function CashDetailModal({ setClose }) {
   return (
@@ -9,8 +10,8 @@ function CashDetailModal({ setClose }) {
         <span>무지출 데이로</span>
         <span>기록하시겠습니까?</span>
         <layout.FlexCenterRow100>
-        <CashBookNoneBtn background={sVar.bookModalYesBtn}>예</CashBookNoneBtn>
-        <CashBookNoneBtn background={sVar.bookModalNoBtn}>아니오</CashBookNoneBtn>
+        <style.CashBookNoneBtn background={sVar.bookModalYesBtn}>예</style.CashBookNoneBtn>
+        <style.CashBookNoneBtn background={sVar.bookModalNoBtn}>아니오</style.CashBookNoneBtn>
         </layout.FlexCenterRow100>
       </style.ModalDefault>
     </style.ModalOverlay>
