50bc03db21251aa5ef2893d9de22c2d54b6c3a62
Danee12 <9356471@gmail.com>
2023-07-03 21:13:48 +0900

    가계부 invalidation 수정 중

--- src/pages/CashBookAdd.jsx
+++ src/pages/CashBookAdd.jsx
@@ -53,11 +53,11 @@ function CashBookAdd({ isMobile, headerHeight, navHeight, mainHeight }) {
   };
 
   // 저장하기 버튼 클릭
-  const queryClient = useQueryClient();
+  const queryClient = new useQueryClient();
   const mutationAddCard = useMutation(CashBookAPI.postCardAdd, {
     onSuccess: (response) => {
       navigate("/cash-book");
-      queryClient.invalidateQueries({ queryKey: ['cashCard'] });
+      queryClient.invalidateQueries('cashCard');
     },
     onError: () => alert("카드 추가에 실패하였습니다."),
   });
