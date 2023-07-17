192aea83405bfa58a26402fe119a45d45d7b34bf
dev-deeplake <127172647+dev-deeplake@users.noreply.github.com>
2023-07-17 23:31:10 +0900

    로그인시 안내문구 위치 수정

--- src/components/ui/effect/LoggedYet.jsx
+++ src/components/ui/effect/LoggedYet.jsx
@@ -3,14 +3,12 @@ import styled from 'styled-components'
 
 function LoggedYet() {
     const LoginOrSignup = styled.div`
-        position: absolute;
-        bottom: 42%;
-        z-index: 2;
         font-size: 0.8rem;
+        margin-top: 20px;
   `
   return (
     <LoginOrSignup>
-        <a href="/login">로그인</a> 또는 <a href="/signup">회원가입</a>
+      <a href="/login">로그인</a> 또는 <a href="/signup">회원가입</a>
     </LoginOrSignup>
   )
 }
