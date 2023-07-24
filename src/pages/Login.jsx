5b7c05f3bdaf3bcebcb42e346a6813e8b46527b1
dev-deeplake <127172647+dev-deeplake@users.noreply.github.com>
2023-07-24 20:35:55 +0900

    로그인 테스트중

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -70,8 +70,12 @@ function Login() {
   const mutationLogin = useMutation(AuthAPI.postLogIn, {
     onSuccess: (response) => {
       alert("로그인이 완료되었습니다.")
+      console.log("login-response:::", response)
+      localStorage.setItem("accessToken", response.data.accessToken);
+      localStorage.setItem("refreshToken", response.data.refreshToken);
+      localStorage.setItem("userId", response.headers.userId);
+      localStorage.setItem("nickname", response.headers.usernickname);
       // console.log(decodeURIComponent(response.headers["usernickname"]))
-      saveUserInfo(response.headers["userid"], response.headers["usernickname"])
       navigate("/") // 회원가입 완료시 메인 이동
     },
     onError: () => alert("로그인이 실패하였습니다.")
