3e1949badab71e98f69ea6f67352f6ab7267cfec
dev-deeplake <127172647+dev-deeplake@users.noreply.github.com>
2023-07-26 00:36:55 +0900

    게시판 상세 댓글 모달 수정

--- src/components/ui/modal/CashDetailModal.jsx
+++ src/components/ui/modal/CashDetailModal.jsx
@@ -8,7 +8,7 @@ function CashDetailModal({ setClose, onClickHandler, children }) {
   return (
     <style.ModalOverlay onClick={setClose}>
       <style.ModalDefault style={{width: "90%"}} onClick={(event) => event.stopPropagation()}>
-        {children.map((ment)=><span>{ment}</span>)}
+        {children.map((ment)=> { return <span>{ment}</span> })}
         <layout.FlexCenterRow100 style={{gap:"10px"}}>
         <style.CashBookNoneBtn background={sVar.bookModalYesBtn} onClick={onClickHandler}>예</style.CashBookNoneBtn>
         <style.CashBookNoneBtn background={sVar.bookModalNoBtn} onClick={setClose}>아니오</style.CashBookNoneBtn>
