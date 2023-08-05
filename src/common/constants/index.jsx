5cca05ed7e098839f8cc11defed5c44792e31092
dev-deeplake <127172647+dev-deeplake@users.noreply.github.com>
2023-08-05 09:45:53 +0900

    yarn build error에 대흥하기 위한 index.js  확장자 변경 (jsx)

---
+++ src/common/constants/index.jsx
@@ -0,0 +1,13 @@
+export { CATEGORY_LIST } from './category';
+export { COMMENT } from './comment';
+export { MENU_LIST } from './menu';
+export { STANDARD_SIZE } from './contextValue';
+export { mainDummyData, cashbookDummyData } from './dummyData';
+// export {
+//   COMMENT,
+//   mainDummyData,
+//   cashbookDummyData,
+//   MENU_LIST,
+//   STANDARD_SIZE,
+//   CATEGORY_LIST,
+// };
